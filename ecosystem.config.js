26959566349a7ef7730339ca09dc505ae20eee2f
ccfine <469781947@qq.com>
2019-06-10 19:58:01 +0800

    完善pm2配置细节

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: "download",
     script: "./server/server.js"
   }],
-  deploy : {
-    production : {
-      user : "root",
-      host : "129.204.68.85",
-      ref  : "origin/master",
-      repo : "https://github.com/ccfine/download-software.git",
-      path : "/ccfine/deployment/download-software",
-      "post-deploy" : "yarn && pm2 reload ecosystem.config.js --env production"
+  deploy: {
+    production: {
+      user: "root",
+      host: "129.204.68.85",
+      ref: "origin/master",
+      repo: "https://github.com/ccfine/download-software.git",
+      path: "/ccfine/deployment/download-software",
+      "post-deploy": "yarn && pm2 reload ecosystem.config.js"
     }
   }
 };
